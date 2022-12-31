1e09d81ad3450c8841374bee762e655acb1c98e4
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2022-12-31 13:40:49 -0300

    Subida de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { FilesService } from './services/files.service';
 export class AppComponent {
   imgParent = '';
   showImg = true;
+  imgRta = '';
 
   constructor(
     private userService: UsersService,
@@ -36,7 +37,20 @@ export class AppComponent {
         console.log(data)
       })
   }
+
   dowloandPdf() {
     this.fileService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdd').subscribe()
+  }
+
+  onUpload(event: Event) {
+    const element = event?.target as HTMLInputElement;
+    const file = element.files?.item(0);
+
+    if (file)
+      this.fileService.uploadFile(file).subscribe(
+        response => {
+          this.imgRta = response.location;
+        }
+      )
   }
 }
--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -2,11 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { saveAs } from 'file-saver';
 import { map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { File } from '../models/file.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
+
+  private apiUrl = `${environment.API_URL}/api/files`;
 
   constructor(
     private http: HttpClient
@@ -21,5 +25,15 @@ export class FilesService {
       }),
       map(() => true)
     )
+  }
+
+  uploadFile(file: Blob) {
+    const dto = new FormData();
+    dto.append('file', file)
+    return this.http.post<File>(`${this.apiUrl}/upload`, dto, {
+      //  headers: {
+      //    "Content-type": "multipart/form-data"
+      //  }
+    })
   }
 }
