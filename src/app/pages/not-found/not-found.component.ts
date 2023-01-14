02bd51a313526b5701300efbd873098903226564
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-01-14 20:22:34 -0300

    Not found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,14 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
+  template: `<p>Not found</p>`,
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NotFoundComponent {
 
 }
