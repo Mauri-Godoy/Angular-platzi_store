a0a948b97a275e3f1ef7657235e6a17f70e637fa
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-01-25 14:25:25 -0300

    Update custom-preload.service.ts

--- src/app/services/custom-preload.service.ts
+++ src/app/services/custom-preload.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
-import { loadavg } from 'os';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
