a82ffe19d6a27a0ec7bcd0d79ea04a46c3d617c6
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-01-25 14:12:14 -0300

    Precargas de módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 import { CmsModule } from './cms/cms.module';
+import { CustomPreloadService } from './services/custom-preload.service';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
+    data: {
+      preload: true
+    }
   },
   {
     path: 'cms',
@@ -20,7 +24,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
